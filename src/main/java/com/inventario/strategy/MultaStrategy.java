2850aa2629c3226d5278532cdf3945ec6ccbdb8b
Gabriel Torres <gabrielstorres08@gmail.com>
2025-05-13 00:13:06 -0300

    Delete src/main/java/com/inventario/strategy directory

--- src/main/java/com/inventario/strategy/MultaStrategy.java
+++
@@ -1,8 +0,0 @@
-package com.inventario.strategy;
-
-import com.inventario.model.Reserva;
-import java.math.BigDecimal;
-
-public interface MultaStrategy {
-    BigDecimal calcular(Reserva reserva);
-}

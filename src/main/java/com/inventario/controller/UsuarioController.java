d6ce990f5bad2ba65268ee6978bf88cf6fa68be8
Gabriel Torres <gabrielstorres08@gmail.com>
2025-05-13 00:04:50 -0300

    Update UsuarioController.java

--- src/main/java/com/inventario/controller/UsuarioController.java
+++ src/main/java/com/inventario/controller/UsuarioController.java
@@ -2,15 +2,20 @@ package com.inventario.controller;
 
 import com.inventario.facade.SistemaInventarioFacade;
 import com.inventario.model.Usuario;
+import com.inventario.service.UsuarioService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+
 @Controller
 public class UsuarioController {
+    UsuarioService usuarioService;
 
     @Autowired private SistemaInventarioFacade sistemaFacade;
 
@@ -19,6 +24,8 @@ public class UsuarioController {
         model.addAttribute("usuarios", sistemaFacade.listarUsuarios());
         return "usuarios";
     }
+
+    
 
     @GetMapping("/usuarios/add")
     public String formNovoUsuario(Model model) {

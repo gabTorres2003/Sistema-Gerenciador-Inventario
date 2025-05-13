06866cec4d77e5dd211b0dc339ae490ed0fe28fa
Gabriel Torres <gabrielstorres08@gmail.com>
2025-05-13 00:12:04 -0300

    Delete src/main/java/com/inventario/service directory

--- src/main/java/com/inventario/service/AuditoriaService.java
+++
@@ -1,9 +0,0 @@
-package com.inventario.service;
-
-import com.inventario.model.Auditoria;
-import java.util.List;
-
-public interface AuditoriaService {
-    void salvar(Auditoria auditoria);
-    List<Auditoria> listarTodos();
-}
